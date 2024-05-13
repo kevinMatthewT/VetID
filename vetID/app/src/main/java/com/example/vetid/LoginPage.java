8cdde83f76e8a4b00fd2e7efb62f8826251a2170
kevin matthew T <kmturbobro02@gmail.com>
2024-05-13 09:10:25 +0700

    update for login page

--- vetID/app/src/main/java/com/example/vetid/LoginPage.java
+++ vetID/app/src/main/java/com/example/vetid/LoginPage.java
@@ -3,12 +3,42 @@ package com.example.vetid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginPage extends AppCompatActivity {
 
+    EditText emailUsername,userPassword;
+    Button login;
+    TextView forgotPassword, signUpRedirect;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+
+        emailUsername =findViewById(R.id.emailForm);
+        userPassword= findViewById(R.id.emailForm);
+        login =findViewById(R.id.loginButton);
+        forgotPassword =findViewById(R.id.forgotPassword);
+        signUpRedirect =findViewById(R.id.signUp);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username=emailUsername.getText().toString();
+                String password=userPassword.getText().toString();
+
+                if(username.length()==0|| password.length()==0){
+                    Toast.makeText(getApplicationContext(),"Fill all of the details to login",Toast.LENGTH_SHORT).show();
+                }else{
+
+                    Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
