7a6190aec2e56bdd41b16f4df73fb2dc2a3706c1
Carmen1104 <carmencongko11@gmail.com>
2024-05-31 22:20:20 +0700

    minor changes

--- app/src/main/java/com/example/vettest/MainActivity.java
+++ app/src/main/java/com/example/vettest/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.vettest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
